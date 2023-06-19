4f9c0c47892b1e277346f264557000605a2bbd7e
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-19 17:23:15 +0200

    added and structured CardProducts component and SingleCard component

---
+++ src/components/CardProducts.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+import SingleCard from './SingleCard.vue'
+export default {
+    name:'CardProducts',
+    components:{
+        SingleCard
+    },
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
