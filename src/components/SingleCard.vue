885d24915a0fe8217da0553214a55975b816db78
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-19 17:30:27 +0200

    structured SingleCard component

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -1,6 +1,15 @@
 <template>
-    <div>
+    <div class="card">
+        <div class="img-wrapper">
+
+        </div>
         
+        <div class="card-title">
+
+        </div>
+        <div class="card-type">
+            
+        </div>
     </div>
 </template>
 <script>
