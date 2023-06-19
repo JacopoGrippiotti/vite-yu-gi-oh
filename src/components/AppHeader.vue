255663aa7ba7a137bb9382c9eb1cec8a933a943a
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-19 16:36:27 +0200

    structured AppHeader component and added AppMain component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name:'AppHeader'
+}
+</script>
+<style lang="scss" scoped>
+    @use '../styles/partials/mixins' as*;
+    @use '../styles/partials/variables' as*;
+</style>
