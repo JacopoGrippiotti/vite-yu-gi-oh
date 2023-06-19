544bf6e380658b8992ed5ca03a7d189248fb71b3
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-19 16:46:47 +0200

    imported AppHeader and AppMain components in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script>
-
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue'
 export default{
-  name:'App'
+  name:'App',
+  components:{
+    AppHeader,
+    AppMain
+  }
 }
 </script>
 
 <template>
-  
+  <AppHeader/>
+  <AppMain/>
 </template>
 
 <style lang="scss">
